0df1e1030eb4de05d83764cf29ea05bb9306dff8
UCUP-PC <yusupubl14@gmail.com>
2019-05-21 10:20:27 +0700

    implement cari produk by id

--- src/main/java/com/example/demo/controller/ProductApiController.java
+++ src/main/java/com/example/demo/controller/ProductApiController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,11 @@ public class ProductApiController {
     @GetMapping("/")
     public Page<Product> findProducts(Pageable page){
         return productDao.findAll(page);
+    }
+    
+    @GetMapping("/{id}")
+    public Product findByid(@PathVariable("id") Product p){
+//        Product product = productDao.findById(id); tidak perlu lagi, karna sudah disediakan Spring Data JPA
+        return p;
     }
 }
